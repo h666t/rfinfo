94734739709875a4fdad6cffd3b487ce24faa92a
h666t <1845720928@qq.com>
2022-05-22 23:11:47 +0800

    commit_message

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    MY_ENV_VAR: process.env.NODE_ENV === 'production' ? 'prod' : 'dev',
+    MY_API_PREFIX: process.env.NODE_ENV === 'production' ? "" : "http://localhost:3000"
+}
 }
 
 module.exports = nextConfig
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
+// import { PrismaClient, Prisma } from '@prisma/client'
+//
+// const prisma = new PrismaClient()
 type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  res.setHeader('content-type', 'application/json; charset=utf-8');
+  // const user = await prisma.user.create({
+  //   data: {
+  //     email: 'huang@prisma.io',
+  //     name: 'Elsa Prisma',
+  //   },
+  // })
+  console.log(2);
+  // console.log(user);
+
   res.status(200).json({ name: 'John Doe' })
 }
