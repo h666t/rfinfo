4ff9e3abead92e90493a3971a87390c6c1fbedbd
h666t <1845720928@qq.com>
2022-05-24 13:59:23 +0800

    commit_message

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import axios from 'axios';
+import helperStyles from "../styles/helper.module.css"
 
  export const getServerSideProps: GetServerSideProps = async ({req})=>{
   // let x = await axios.get(`${process.env.MY_API_PREFIX}/api/hello`)
@@ -18,9 +19,14 @@ const Home: NextPage = (prop) => {
   return (<>
     <div className={styles.topBar}>
         <div className={styles.logo}></div>
-        <div className={'menu'}></div>
+        <div className={styles.menu}></div>
     </div>
-    <div className={styles.firstScreen}></div>
+    <div className={styles.firstScreen}>
+      <div className={styles.firstScreenTitleContainer}>
+        <div className={`${helperStyles.oneLine} ${styles.title}`}>文字文字文字文字文字文字文字文字文字文字文字文字文字文字</div>
+        <div className={styles.description}>愿所有的美好如约而至，愿所有的黑暗都能看到希望，我们都能微笑前行，人生没有完美，有些遗憾才美...</div>
+      </div>
+    </div>
   </>)
 }
 
